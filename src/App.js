cef42d71bb4c878d3a98efb2dabb69ddb1d424ab
maxillect <maxillect@gmail.com>
2021-09-24 15:05:53 +0530

    theme updated

---
+++ src/App.js
@@ -0,0 +1,21 @@
+/** @format */
+
+import logo from "./logo.svg";
+
+function App() {
+	return;
+	<>
+		<div class="mb-3">
+			<label for="exampleFormControlTextarea1" class="form-label">
+				Enter Text Below
+			</label>
+			<textarea
+				class="form-control"
+				id="exampleFormControlTextarea1"
+				rows="3"
+			></textarea>
+		</div>
+	</>;
+}
+
+export default App;
